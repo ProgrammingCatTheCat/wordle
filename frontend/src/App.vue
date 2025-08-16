fc88b91eaec8384d8d60fdd48de9e747eb34a34c
Angie <2507263949@qq.com>
2025-08-16 21:13:59 +0800

    init project

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="header">
+      <p>header header ... </p>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+   
+</style>
